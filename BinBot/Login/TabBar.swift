ce8b177133ff48f6fdb96b45200cb3e29b44c394
Maria <89463561+marrrydm@users.noreply.github.com>
2023-09-08 17:41:35 +0400

    tabbar

---
+++ BinBot/Login/TabBar.swift
@@ -0,0 +1,68 @@
+import UIKit
+
+class TabBarController: UITabBarController, UITabBarControllerDelegate {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        self.delegate = self
+        self.tabBar.backgroundColor = UIColor(red: 0.141, green: 0.149, blue: 0.184, alpha: 1)
+        self.tabBar.tintColor = UIColor(red: 0.141, green: 0.149, blue: 0.184, alpha: 1)
+        self.tabBar.barTintColor = UIColor(red: 0.141, green: 0.149, blue: 0.184, alpha: 1)
+        self.tabBar.layer.borderWidth = 0.5
+        self.tabBar.layer.borderColor = UIColor(red: 0.182, green: 0.197, blue: 0.254, alpha: 1).cgColor
+
+        let appearance = UITabBarItem.appearance()
+        let attributes = [
+            NSAttributedString.Key.font:
+                UIFont.systemFont(ofSize: 10, weight: .semibold),
+            NSAttributedString.Key.foregroundColor: UIColor(red: 0.584, green: 0.612, blue: 0.663, alpha: 1)
+        ]
+
+        appearance.setTitleTextAttributes(attributes as [NSAttributedString.Key : Any], for: .normal)
+
+        let attributesSelected = [
+            NSAttributedString.Key.font:
+                UIFont.systemFont(ofSize: 10, weight: .semibold),
+            NSAttributedString.Key.foregroundColor: UIColor(red: 1, green: 0.855, blue: 0.267, alpha: 1)
+        ]
+
+        appearance.setTitleTextAttributes(attributesSelected as [NSAttributedString.Key : Any], for: .selected)
+    }
+
+    private func getController() -> UIViewController {
+        return UserData.showedTrade ? PlatformController() : LoginVC()
+    }
+
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        let navBar = UINavigationController(rootViewController: getController())
+        navBar.navigationBar.setBackgroundImage(UIImage(), for: .default)
+        navBar.navigationBar.shadowImage = UIImage()
+        navBar.navigationBar.isTranslucent = true
+        let tradeControllerItem = UITabBarItem(title: "tabbar.platform".localize(), image: UIImage(named: "platform")?.withRenderingMode(.alwaysOriginal), selectedImage: UIImage(named: "platform_selected")?.withRenderingMode(.alwaysOriginal))
+        navBar.tabBarItem = tradeControllerItem
+
+        let botController = UINavigationController(rootViewController: BotController())
+        botController.navigationBar.setBackgroundImage(UIImage(), for: .default)
+        botController.navigationBar.shadowImage = UIImage()
+        botController.navigationBar.isTranslucent = true
+        let robotControllerItem = UITabBarItem(title: "tabbar.robot".localize(), image: UIImage(named: "robot")?.withRenderingMode(.alwaysOriginal), selectedImage: UIImage(named: "robot_selected")?.withRenderingMode(.alwaysOriginal))
+        botController.tabBarItem = robotControllerItem
+
+        let supportController = UINavigationController(rootViewController: SupportController())
+        supportController.navigationBar.setBackgroundImage(UIImage(), for: .default)
+        supportController.navigationBar.shadowImage = UIImage()
+        supportController.navigationBar.isTranslucent = true
+        let supportControllerItem = UITabBarItem(title: "tabbar.support".localize(), image: UIImage(named: "support")?.withRenderingMode(.alwaysOriginal), selectedImage: UIImage(named: "support_selected")?.withRenderingMode(.alwaysOriginal))
+
+        supportController.tabBarItem = supportControllerItem
+
+        let accountController = AccountController()
+        let accountControllerItem = UITabBarItem(title: "tabbar.account".localize(), image: UIImage(named: "account")?.withRenderingMode(.alwaysOriginal), selectedImage: UIImage(named: "account_selected")?.withRenderingMode(.alwaysOriginal))
+
+        accountController.tabBarItem = accountControllerItem
+
+        self.viewControllers = [navBar, botController, supportController, accountController]
+    }
+}
--- BinBot/ViewController.swift
+++ BinBot/ViewController.swift
@@ -29,3 +29,24 @@ class UnderlinedLabel: UILabel {
         }
     }
 }
+
+extension UIViewController {
+    func showToast(message : String) {
+        let toastLabel = UILabel(frame: CGRect(x: self.view.frame.size.width / 2.0 - 140.0, y: self.view.frame.size.height / 2.0 - 18.0, width: 280.0, height: 56))
+        toastLabel.backgroundColor = UIColor.black.withAlphaComponent(0.6)
+        toastLabel.textColor = UIColor.white
+        toastLabel.font = .systemFont(ofSize: 14)
+        toastLabel.textAlignment = .center;
+        toastLabel.text = message
+        toastLabel.alpha = 1.0
+        toastLabel.layer.cornerRadius = 12;
+        toastLabel.numberOfLines = 2
+        toastLabel.clipsToBounds  =  true
+        self.view.addSubview(toastLabel)
+        UIView.animate(withDuration: 1.0, delay: 1.5, options: .curveEaseOut, animations: {
+            toastLabel.alpha = 0.0
+        }, completion: {(isCompleted) in
+            toastLabel.removeFromSuperview()
+        })
+    }
+}
